5f1991b373129de137720257fb156c66f0683f90
root <3203317@qq.com>
2017-09-15 18:08:58 +0800

    haha

--- settings.js
+++ settings.js
@@ -10,13 +10,13 @@ module.exports = {
     mysql: {
       database: 'oauth2',
       host: '127.0.0.1',
-      port: 22306,
+      port: 12306,
       user: 'root',
-      password: 'password',
+      password: '123456',
       connectionLimit: 50
     },
     redis: {
-      port: 22379,
+      port: 12379,
       host: '127.0.0.1',
       password: '123456'
     }
@@ -25,9 +25,9 @@ module.exports = {
     mysql: {
       database: 'foreworld',
       host: '127.0.0.1',
-      port: 22306,
+      port: 12306,
       user: 'root',
-      password: 'password',
+      password: '123456',
       connectionLimit: 50
     }
   }
